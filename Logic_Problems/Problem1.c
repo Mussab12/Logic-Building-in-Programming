095112ff75e95e9e589db2023a3d12d5eaeb9c7d
Mussab Bin Shahid <49309717+Mussab12@users.noreply.github.com>
2022-07-07 17:56:37 +0500

    Add files via upload

---
+++ Logic_Problems/Problem1.c
@@ -0,0 +1,36 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int mul;
+    int sum=0;
+    int n;
+    cout<<"Enter natural numbers"<<endl;
+    cout<<"Enter numbers 3 or 5"<<endl;
+    cin>>n;
+    for(int i=0;i<1000;i++)
+    {
+        cout<<i;
+         mul=n*i;
+            
+        if(mul%3==0 || mul%5==0) // checking if the number is divisible by 3 or 5
+        {
+            sum=sum+mul;
+        }
+        {
+            sum=sum+mul;
+           
+            cout<<"Mul of 3"<<endl;
+        }
+        else
+        {
+            cout<<"please enter correct number"<<endl;
+        }
+    }
+    
+   cout<<sum;
+    
+    return 0;
+}
